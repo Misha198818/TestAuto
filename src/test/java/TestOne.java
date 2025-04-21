b7c7566817a8cbc8cd561d1a2d063656e442fc58
Михаил <mixail.anishin@mail.ru>
2025-04-21 13:19:10 +0300

    модуль 12

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,17 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
+
+import pages.TestViki;
 
 public class TestOne extends BaseTest{
 
+    @DisplayName("Википедия")
     @Test
-    public void TestPage(){
-        Selenide.open("https://ru.m.wikipedia.org/wiki/");
-        Selenide.$x("//input[@class='search skin-minerva-search-trigger']")
-                                                    .click();
-        Selenide.$x("//input[@class='search mf-icon-search']")
-                                                    .setValue("Java")
-                                                    .pressEnter();               
-        Selenide.$x("//img[@alt='Википедия']").shouldBe(Condition.visible);
-    }
+    public void Test(){
+        TestViki testViki = new TestViki();
+        testViki.openPage()
+                .inputRequest()
+                .eleventVisible();
+    }  
 }
